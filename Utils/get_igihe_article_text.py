c68ef7b5398a449318b297f9446f852fb277dda7
bahatiphill <gur79tub@gmail.com>
2019-12-11 20:49:56 +0200

    now storing all scrapped text in txt files

--- Utils/get_igihe_article_text.py
+++ Utils/get_igihe_article_text.py
@@ -1,18 +1,22 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
-def get_igihe_article_content(url):
+def get_igihe_article_content(url, folder_to_write_to):
     '''
     This function get igihe article content and save the content on the txt file
     '''
 
     response =  requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    
-    article =  soup.find_all('div', class_="inkuru-title-2")[0]
+    try :
+        article =  soup.find_all('div', class_="inkuru-title-2")[0]
+    except IndexError as e:
+        print('Another IndexError: ', e)
+        return 
     paragraphs = article.find_all('p')
-    filename = url.rsplit('/', 1)[-1]
+    filename = url.rsplit('/', 1)[-1] + '.txt'
 
-    with open(filename + '.txt', 'w+') as f:
+    with open(os.path.join(folder_to_write_to, filename), 'w+') as f:
         for para in paragraphs:
             f.write(para.getText()+ '\n \n')
--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 import argparse
 
+
 from Utils.igihe_latest_links import get_latest_igihe_links
+from Utils.get_igihe_article_text import get_igihe_article_content
 
 parser =  argparse.ArgumentParser(description="Kinyarwanda texts Scrapper")
 parser.add_argument('site', help="Site to scrape. Choose between (igihe, inyarwanda, umuseke, umubyeyi)", choices=['igihe', 'inyarwanda', 'umuseke'])
@@ -8,7 +11,10 @@ parser.add_argument('-a', '--articles', help="Number of article to scrape", type
 
 args = parser.parse_args()
 
+#Place to store all scraped links and texts
+DATAPATH = 'DATA'
 
+ROOTFORLDER = os.getcwd()
 
 def main():
     
@@ -18,7 +24,7 @@ def main():
     new_links = []
 
     #Write them on file
-    with open('DATA/igihelinks.txt', 'r+') as fr:
+    with open(os.path.join(ROOTFORLDER, DATAPATH, 'links', 'igihelinks.txt'), 'r+') as fr:
         links_in_the_file =  fr.readlines()
         
     new_igihe_links =  [ link.strip() for link in new_igihe_links ]
@@ -28,8 +34,17 @@ def main():
         if link not in links_in_the_file:
             new_links.append(link)
 
-    with open('DATA/igihelinks.txt', 'a') as fa:
+    #Write new Links to the file
+    with open(os.path.join(ROOTFORLDER, DATAPATH, 'links','igihelinks.txt'), 'a') as fa:
         for item in new_links:
             fa.write(item+ '\n')
+
+    
+    #Scrape All newest links
+    for item in new_links:
+        get_igihe_article_content(item, os.path.join(ROOTFORLDER, DATAPATH, 'texts'))
+
+
+    print('DONE')
 
 main()
